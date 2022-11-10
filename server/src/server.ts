1aa44c1ea36155fc85288d2bf707975976106ebe
Lorenzo Giacomo <lorenzogiacomo1@gmail.com>
2022-11-10 16:47:51 -0300

    prisma config

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,11 @@
 import Fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+import cors from '@fastify/cors'
+
+const prisma = new PrismaClient({
+  // prisma prints all the querys in the database. Important to debug.
+  log: ['query'],
+})
 
 // first function that execute in the code.
 async function bootstrap() {
@@ -7,11 +14,18 @@ async function bootstrap() {
     logger: true,
   })
 
-  fastify.get('/pools/count', () => {
-    return { count: 12312 }
+  await fastify.register(cors, {
+  // allows any application to access the server.
+    origin: true,
   })
 
-  await fastify.listen({ port: 3333 })
+  fastify.get('/pools/count', async () => {
+    // findMany is simillar to select in sql
+  const pools = await prisma.pool.count()
+    return { pools }
+  })
+
+  await fastify.listen({ port: 3333, host: '0.0.0.0' })
 }
 
 bootstrap()
